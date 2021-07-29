6d679f062c8d98a5316d0395311d7c71c641e17c
Jessica McInchak <jessica.mcinchak@gmail.com>
2021-07-29 10:13:41 +0200

    maybe not one big file this time

--- src/my-map.ts
+++ src/my-map.ts
@@ -1,19 +1,10 @@
 import { css, customElement, html, LitElement, property } from "lit-element";
 import stylefunction from "ol-mapbox-style/dist/stylefunction";
-import { MVT } from "ol/format";
-import { Tile as TileLayer } from "ol/layer";
-import VectorTileLayer from "ol/layer/VectorTile";
 import Map from "ol/Map";
 import { fromLonLat, transformExtent } from "ol/proj";
-import { OSM, XYZ } from "ol/source";
-import { ATTRIBUTION } from "ol/source/OSM";
-import VectorTileSource from "ol/source/VectorTile";
 import View from "ol/View";
 
-// Ordnance Survey sources
-const tileServiceUrl = `https://api.os.uk/maps/raster/v1/zxy/Light_3857/{z}/{x}/{y}.png?key=${import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY}`;
-const vectorTileServiceUrl = `https://api.os.uk/maps/vector/v1/vts/tile/{z}/{y}/{x}.pbf?srs=3857&key=${import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY}`;
-const vectorTileStyleUrl = `https://api.os.uk/maps/vector/v1/vts/resources/styles?srs=3857&key=${import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY}`;
+import { rasterBaseMap, osVectorTileBaseMap } from "./os-layers";
 
 @customElement("my-map")
 export class MyMap extends LitElement {
@@ -45,41 +36,18 @@ export class MyMap extends LitElement {
   // runs after the initial render
   firstUpdated() {
     const target = this.shadowRoot!.querySelector("#map") as HTMLElement;
-
-    const rasterBaseMap = new TileLayer({
-      source: import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY
-        ? new XYZ({
-            url: tileServiceUrl,
-            attributions: [
-              "© Crown copyright and database rights 2021 OS (0)100019252",
-            ],
-            attributionsCollapsible: false,
-            maxZoom: 20,
-          })
-        : // No OrdnanceSurvey key found, sign up for free here https://osdatahub.os.uk/plans
-          new OSM({
-            attributions: [ATTRIBUTION],
-          }),
-    });
     
-    const osVectorTileBaseMap = new VectorTileLayer({
-      declutter: true,
-      source: new VectorTileSource({
-        format: new MVT(),
-        url: vectorTileServiceUrl,
-        attributions: [
-          "© Crown copyright and database rights 2021 OS (0)100019252",
-        ],
-        attributionsCollapsible: false,
-      }),
-    });
-    
+    // apply style to OS vector tile layer if applicable
     // ref https://github.com/openlayers/ol-mapbox-style#usage-example
-    fetch(vectorTileStyleUrl)
-      .then((response) => response.json())
-      .then((glStyle) => stylefunction(osVectorTileBaseMap, glStyle, "esri"))
-      .catch((error) => console.log(error));
+    if (import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY && osVectorTileBaseMap) {
+      const vectorTileStyleUrl = `https://api.os.uk/maps/vector/v1/vts/resources/styles?srs=3857&key=${import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY}`;
 
+      fetch(vectorTileStyleUrl)
+        .then((response) => response.json())
+        .then((glStyle) => stylefunction(osVectorTileBaseMap, glStyle, "esri"))
+        .catch((error) => console.log(error));
+    }
+    
     new Map({
       target,
       layers: [import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY ? osVectorTileBaseMap : rasterBaseMap], // maybe a @property ENUM in future?
