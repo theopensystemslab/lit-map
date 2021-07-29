bacc0a6b67b50bbadc8f9596460fafc6b0da0cdf
Jessica McInchak <jessica.mcinchak@gmail.com>
2021-07-29 09:28:07 +0200

    set env vars, render os base maps

--- src/my-map.ts
+++ src/my-map.ts
@@ -1,9 +1,19 @@
 import { css, customElement, html, LitElement, property } from "lit-element";
-import TileLayer from "ol/layer/Tile";
+import stylefunction from "ol-mapbox-style/dist/stylefunction";
+import { MVT } from "ol/format";
+import { Tile as TileLayer } from "ol/layer";
+import VectorTileLayer from "ol/layer/VectorTile";
 import Map from "ol/Map";
 import { fromLonLat, transformExtent } from "ol/proj";
-import OSM from "ol/source/OSM";
+import { OSM, XYZ } from "ol/source";
+import { ATTRIBUTION } from "ol/source/OSM";
+import VectorTileSource from "ol/source/VectorTile";
 import View from "ol/View";
+
+// Ordnance Survey sources
+const tileServiceUrl = `https://api.os.uk/maps/raster/v1/zxy/Light_3857/{z}/{x}/{y}.png?key=${import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY}`;
+const vectorTileServiceUrl = `https://api.os.uk/maps/vector/v1/vts/tile/{z}/{y}/{x}.pbf?srs=3857&key=${import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY}`;
+const vectorTileStyleUrl = `https://api.os.uk/maps/vector/v1/vts/resources/styles?srs=3857&key=${import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY}`;
 
 @customElement("my-map")
 export class MyMap extends LitElement {
@@ -36,13 +46,43 @@ export class MyMap extends LitElement {
   firstUpdated() {
     const target = this.shadowRoot!.querySelector("#map") as HTMLElement;
 
+    const rasterBaseMap = new TileLayer({
+      source: import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY
+        ? new XYZ({
+            url: tileServiceUrl,
+            attributions: [
+              "© Crown copyright and database rights 2021 OS (0)100019252",
+            ],
+            attributionsCollapsible: false,
+            maxZoom: 20,
+          })
+        : // No OrdnanceSurvey key found, sign up for free here https://osdatahub.os.uk/plans
+          new OSM({
+            attributions: [ATTRIBUTION],
+          }),
+    });
+    
+    const osVectorTileBaseMap = new VectorTileLayer({
+      declutter: true,
+      source: new VectorTileSource({
+        format: new MVT(),
+        url: vectorTileServiceUrl,
+        attributions: [
+          "© Crown copyright and database rights 2021 OS (0)100019252",
+        ],
+        attributionsCollapsible: false,
+      }),
+    });
+    
+    // ref https://github.com/openlayers/ol-mapbox-style#usage-example
+    fetch(vectorTileStyleUrl)
+      .then((response) => response.json())
+      .then((glStyle) => stylefunction(osVectorTileBaseMap, glStyle, "esri"))
+      .catch((error) => console.log(error));
+
     new Map({
       target,
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
+      layers: [import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY ? osVectorTileBaseMap : rasterBaseMap], // maybe a @property ENUM in future?
       view: new View({
         projection: "EPSG:3857",
         extent: transformExtent(
